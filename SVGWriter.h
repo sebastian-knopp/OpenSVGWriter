dad5cd1f2573f169fe813f82f0cc0cd162ce13db
Sebastian Knopp <abdlbitar@gmail.com>
2015-03-26 10:45:22 +0100

    updated header

--- SVGWriter.h
+++ SVGWriter.h
@@ -1,5 +1,10 @@
 #pragma once
 
+////////////////////////////////////////////////////////////////////////////////
+// OpenSVGWriter
+// https://github.com/sebastian-knopp/OpenSVGWriter
+////////////////////////////////////////////////////////////////////////////////
+//
 // MIT LICENSE
 // Copyright (c) 2015 Sebastian Knopp
 //
@@ -17,9 +22,10 @@
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 // IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
 
 
 #include <fstream>
