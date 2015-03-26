b7583793f652b44c9dec67fcc4c0997570a8670f
Sebastian Knopp <sbstn.knopp@gmail.com>
2015-03-26 18:03:15 +0100

    allow to draw transparent border only rectangles

--- Demo.cpp
+++ Demo.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 
-int main()
+void runSVGWriterDemo()
 {
     const std::string filename = "demo.html";
 
@@ -22,6 +22,30 @@ int main()
 
     // x-y Coordinates can be given in an arbitraty fashion (here: fromX > toX, fromY > toY)
     writer.drawRectangle(1000, 625, 900, 525, 7);
+}
+
+
+void runMatrixDemo()
+{
+    SVGWriter writer("matrixdemo.html", 300, 20);
+    for (int x = 0; x < 10; ++x)
+    {
+        for (int y = 0; y < 10; ++y)
+        {
+            writer.drawRectangle(11*x, 11*y, 11*x+10, 11*y+10, 5 * x + 17 * y);
+            writer.drawCircle(11*x + 5, 11*y + 5, 13 * x + 7 * y, 9);
+        }
+    }
+
+    writer.drawRectangle(11*2+2, 11*3+2, 11*5+8, 11*3+8, 0);
+    writer.drawRectangle(11*2+5, 11*3+6, 11*5+16, 11*9+17, 7, 5);
+}
+
+
+int main()
+{
+    runSVGWriterDemo();
+    runMatrixDemo();
 
     return 0;
 }
